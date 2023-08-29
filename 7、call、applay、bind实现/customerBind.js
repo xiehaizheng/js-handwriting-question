07858a867fe6460906b211b5e530b8fc45a5a1a5
xiehaizheng <xiehaizheng@meituan.com>
2023-08-29 10:05:24 +0800

    feat:test case

--- 7、call、applay、bind实现/customerBind.js
+++ 7、call、applay、bind实现/customerBind.js
@@ -12,20 +12,44 @@ Function.prototype.customerBind = function (context, ...args) {
   }
 
   return function F(...innerArgs) {
+    console.log('args', ...args);
+    console.log('innerArgs', ...innerArgs);
     // 判断是否是new构造函数
     // 由于这里是调用的call方法，因此不需要判断context是否为空
     return that.call(this instanceof F ? this : context, ...args, ...innerArgs);
   }
 }
 
-
-Function.prototype.bind1 = function (context, ...args) {
-  const that = this;
-  if (typeof that !== 'function') {
-    throw new TypeError('this is not function');
-  }
-
-  return function F(...innerArgs) {
-    return that.call(this instanceof F ? this : context, ...args, ...innerArgs);
-  }
+function list() {
+  return Array.prototype.slice.call(arguments);
 }
+
+function addArguments(arg1, arg2) {
+  return arg1 + arg2;
+}
+
+// var list1 = list(1, 2, 3); // [1, 2, 3]
+
+// var result1 = addArguments(1, 2); // 3
+
+// 创建一个函数，它拥有预设参数列表。
+var leadingThirtysevenList = list.customerBind(null, 37);
+
+// 创建一个函数，它拥有预设的第一个参数
+var addThirtySeven = addArguments.customerBind(null, 37);
+
+var list2 = leadingThirtysevenList();
+// [37]
+console.log(list2);
+
+var list3 = leadingThirtysevenList(1, 2, 3);
+// [37, 1, 2, 3]
+console.log(list3);
+
+// var result2 = addThirtySeven(5);
+// // 37 + 5 = 42
+// console.log(result2);
+
+// var result3 = addThirtySeven(5, 10);
+// // 37 + 5 = 42，第二个参数被忽略
+// console.log(result3);
